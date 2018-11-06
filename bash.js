8de1bd84be09cb07685c15427a2a3abaa889b239
Melanie Hublard Hershman <melhhersh@gmail.com>
2018-11-06 12:47:11 -0500

    Added pwd

--- bash.js
+++ bash.js
@@ -1,13 +1,13 @@
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
-  const cmd  = data.toString().trim();
-  process.stdout.write('You typed: ' + cmd);
+    const cmd  = data.toString().trim();
+    if(cmd === 'pwd'){
+        const pwd = process.cwd();
+        process.stdout.write(pwd);
+    }
+    else{
+        process.stdout.write('You typed: ' + cmd);
+    }
   process.stdout.write('\nprompt > ');
-});
-
-process.stdin.on('pwd', (pwd) => {
-    pwd = process.cwd();
-    process.stdout.write(pwd);
-    process.stdout.write('\nprompt > ')
 });
